76b1a2c51351874226f417238a10a87bc7120605
Sebastian Sobocinski <sebastian.sobocinski.7@gmail.com>
2018-12-19 06:38:05 +0100

    removal of unwanted files

--- StoreReactNET/ClientApp/routes.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import { Route } from 'react-router-dom';
-import { App } from './components/App';
-import { Home } from './components/Home';
-
-export const routes = <Route path='/' component={App} />
-
-
